b5deca11af76ed86da134f8e1f07c2e6d63f63e8
Mykola Ostapovych <ostapovych11@gmail.com>
2025-06-23 18:21:03 +0300

    Initial commit

---
+++ src/main/java/com/medical/management/medlab_system/MedlabSystemApplication.java
@@ -0,0 +1,13 @@
+package com.medical.management.medlab_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlabSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedlabSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medical/management/medlab_system/MedlabSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.management.medlab_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedlabSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
